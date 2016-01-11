fa370ae5735218ee6567493306274df4cbd4cecb
Jeremy Gustie <jgustie@blackducksoftware.com>
2016-01-11 11:43:18 -0500

    COMMONCOMP-43 : Fix NPE in constructor

--- bom/src/test/java/com/blackducksoftware/bom/io/LinkedDataContextTest.java
+++ bom/src/test/java/com/blackducksoftware/bom/io/LinkedDataContextTest.java
@@ -11,7 +11,36 @@
  */
 package com.blackducksoftware.bom.io;
 
+import static com.google.common.truth.Truth.assertThat;
+
+import java.net.URI;
+
+import org.junit.Test;
 
 public class LinkedDataContextTest {
+
+    @Test
+    public void testBaseUri() {
+        String base = "http://example.com/test";
+        LinkedDataContext context = new LinkedDataContext(base);
+        assertThat(context.getBase()).isEqualTo(URI.create(base));
+        assertThat(context.getBase().isAbsolute()).isTrue();
+    }
+
+    @Test
+    public void testNullBaseUri() {
+        assertThat(new LinkedDataContext().getBase()).isNull();
+        assertThat(new LinkedDataContext(null).getBase()).isNull();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidBaseUri() {
+        new LinkedDataContext(":");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testRelativeBaseUri() {
+        new LinkedDataContext("/foo");
+    }
 
 }
