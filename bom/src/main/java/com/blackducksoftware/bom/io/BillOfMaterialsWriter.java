dbef4f7ff4c53d2d029f8e99227dfcf279f2fdcd
Jeremy Gustie <jgustie@blackducksoftware.com>
2016-05-11 11:15:55 -0400

    COMMONCOMP-70 : Switch to Apache 2.0 License

--- bom/src/main/java/com/blackducksoftware/bom/io/BillOfMaterialsWriter.java
+++ bom/src/main/java/com/blackducksoftware/bom/io/BillOfMaterialsWriter.java
@@ -1,13 +1,17 @@
 /*
- * Copyright (C) 2015 Black Duck Software Inc.
- * http://www.blackducksoftware.com/
- * All rights reserved.
+ * Copyright 2015 Black Duck Software, Inc.
  *
- * This software is the confidential and proprietary information of
- * Black Duck Software ("Confidential Information"). You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with Black Duck Software.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.blackducksoftware.bom.io;
 
@@ -18,14 +22,14 @@ import java.io.Flushable;
 import java.io.IOException;
 import java.io.OutputStream;
 
-import rx.Subscriber;
-import rx.functions.Action1;
-
 import com.blackducksoftware.bom.Node;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonGenerator.Feature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.io.ByteSink;
+
+import rx.Subscriber;
+import rx.functions.Action1;
 
 /**
  * A simple writer for producing a JSON formatted Bill of Materials from a source of node instances.
