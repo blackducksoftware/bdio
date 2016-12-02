ccd23e2d7b04b38e21672e85ee77513c242631ac
Jeremy Gustie <jgustie@blackducksoftware.com>
2016-12-02 12:21:32 -0500

    Create a new test project for common testing code

---
+++ bdio2/src/test/java/com/blackducksoftware/bdio2/BdioDocumentBuilderTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2016 Black Duck Software Inc.
+ * http://www.blackducksoftware.com/
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Black Duck Software ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Black Duck Software.
+ */
+package com.blackducksoftware.bdio2;
+
+import java.net.URI;
+
+import org.junit.Test;
+
+/**
+ * Tests for {@link BdioDocument.Builder}.
+ *
+ * @author jgustie
+ */
+public class BdioDocumentBuilderTest {
+
+    @Test(expected = NullPointerException.class)
+    public void builder_nullId() {
+        new BdioDocument.Builder().id(null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void builder_nullCreation() {
+        new BdioDocument.Builder().creation(null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void builder_nullBaseString() {
+        new BdioDocument.Builder().base((String) null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void builder_nullBaseUri() {
+        new BdioDocument.Builder().base((URI) null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void builder_nullAllowRemoteLoading() {
+        new BdioDocument.Builder().allowRemoteLoading(null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void builder_nullExpandAsBdio() {
+        new BdioDocument.Builder().expandAsBdio(null);
+    }
+
+}
