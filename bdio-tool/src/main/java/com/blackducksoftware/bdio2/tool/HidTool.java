9518a09b7bf83e34cfc8d1872b1b732f526591b2
Jeremy Gustie <jgustie@blackducksoftware.com>
2018-05-29 11:39:58 -0400

    HID tool improvements: allow HID's to come in via stdin, allow URI output and sorting

--- bdio-tool/src/main/java/com/blackducksoftware/bdio2/tool/HidTool.java
+++ bdio-tool/src/main/java/com/blackducksoftware/bdio2/tool/HidTool.java
@@ -15,13 +15,18 @@
  */
 package com.blackducksoftware.bdio2.tool;
 
+import static com.blackducksoftware.common.base.ExtraThrowables.illegalArgument;
+import static com.google.common.base.CaseFormat.LOWER_HYPHEN;
+import static com.google.common.base.CaseFormat.UPPER_UNDERSCORE;
 import static com.google.common.base.Preconditions.checkState;
 import static java.util.Collections.emptyList;
 
 import java.net.URISyntaxException;
+import java.nio.charset.Charset;
 import java.util.ArrayDeque;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -32,6 +37,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.function.Function;
 
 import com.blackducksoftware.common.value.HID;
 import com.google.common.base.Strings;
@@ -43,7 +49,50 @@ import com.google.common.base.Strings;
  */
 public class HidTool extends Tool {
 
+    public enum Format implements Function<HID, String> {
+        DEFAULT {
+            @Override
+            public String apply(HID hid) {
+                return hid.toString();
+            }
+        },
+        URI {
+            @Override
+            public String apply(HID hid) {
+                return hid.toUriString();
+            }
+        },
+        TREE {
+            @Override
+            public String apply(HID hid) {
+                return hid.getName();
+            }
+        },
+    }
+
+    public enum Sort implements Comparator<HID> {
+        DEFAULT {
+            @Override
+            public int compare(HID left, HID right) {
+                return 0; // Assume stable sort on input
+            }
+        },
+        IGNORE_PATH {
+            @Override
+            public int compare(HID left, HID right) {
+                return HID.ignorePathOrder().compare(left, right);
+            }
+        },
+        PREORDER {
+            @Override
+            public int compare(HID left, HID right) {
+                return HID.preOrder().compare(left, right);
+            }
+        },
+    }
+
     // TODO Add "isAnscestor" functionality and such for testing HIDs
+    // TODO Maybe a separate "file" or "path" tool for building HIDs or full file nodes off the file system
 
     public static void main(String[] args) {
         new HidTool(null).parseArgs(args).run();
@@ -51,38 +100,62 @@ public class HidTool extends Tool {
 
     private List<String> uris = Collections.emptyList();
 
-    private boolean treeOutput;
+    private Format format = Format.DEFAULT;
+
+    private Sort sort = Sort.DEFAULT;
 
     public HidTool(String name) {
         super(name);
     }
 
-    public void setTreeOutput(boolean treeOutput) {
-        this.treeOutput = treeOutput;
+    public void setFormat(Format format) {
+        this.format = Objects.requireNonNull(format);
+    }
+
+    public void setSort(Sort sort) {
+        this.sort = Objects.requireNonNull(sort);
     }
 
     @Override
     protected void printUsage() {
-        printOutput("usage: %s [--tree] [uri ...]%n", name());
+        printOutput("usage: %s [--tree|--uri] [--sort=ignore-path|preorder] [uri ...]%n", name());
     }
 
     @Override
     protected void printHelp() {
         Map<String, String> options = new LinkedHashMap<>();
         options.put("--tree", "Display all inputs as a combined file tree");
+        options.put("--uri", "Display the URI");
+        options.put("--sort=<order>", "Change the sort order (default is the order of input)");
         printOptionHelp(options);
+    }
+
+    @Override
+    protected boolean isOptionWithArgs(String option) {
+        return super.isOptionWithArgs(option) || option.equals("--sort");
     }
 
     @Override
     protected Tool parseArguments(String[] args) throws Exception {
         for (String arg : options(args)) {
             if (arg.equals("--tree")) {
-                setTreeOutput(true);
+                setFormat(Format.TREE);
+                args = removeFirst(arg, args);
+            } else if (arg.equals("--uri")) {
+                setFormat(Format.URI);
+                args = removeFirst(arg, args);
+            } else if (arg.startsWith("--sort=")) {
+                setSort(optionValue(arg).map(s -> LOWER_HYPHEN.to(UPPER_UNDERSCORE, s)).map(Sort::valueOf)
+                        .orElseThrow(illegalArgument("Invalid sort order: %s", arg)));
                 args = removeFirst(arg, args);
             }
         }
 
         uris = arguments(args);
+        if (uris.isEmpty()) {
+            uris = getInput("-").asCharSource(Charset.defaultCharset()).readLines();
+        }
+
         return super.parseArguments(args);
     }
 
@@ -104,12 +177,10 @@ public class HidTool extends Tool {
     @Override
     protected void execute() throws Exception {
         checkState(!uris.isEmpty(), "input is empty");
-        if (treeOutput) {
+        if (format == Format.TREE) {
             printTree();
         } else {
-            for (String uri : uris) {
-                printOutput("%s%n", HID.from(uri));
-            }
+            uris.stream().map(HID::from).sorted(sort).map(format).forEachOrdered(h -> printOutput("%s%n", h));
         }
     }
 
@@ -144,7 +215,7 @@ public class HidTool extends Tool {
 
             StringBuilder buffer = new StringBuilder();
             TreeFormat.appendAsciiIndent(buffer, depth, childrenAtDepths::contains, hasMoreSiblings);
-            buffer.append(hid.getName());
+            buffer.append(format.apply(hid));
             if (hid.isRoot()) {
                 if (hid.getScheme().equalsIgnoreCase("file")) {
                     buffer.append(" (file system root)");
